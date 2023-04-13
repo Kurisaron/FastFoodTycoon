9a90c2429d8d9b8a183ce15502be6afa9e2950a3
cgcochran <cgcochran@mail.csuchico.edu>
2023-04-13 07:12:43 -0700

    Added More Customer Behavior

--- FastFoodTycoonProject/Assets/Scripts/CustomerController.cs
+++ FastFoodTycoonProject/Assets/Scripts/CustomerController.cs
@@ -5,158 +5,129 @@ using UnityEngine;
 public class CustomerController : MonoBehaviour
 {
     public delegate void CustomerDespawned();
-
     public static event CustomerDespawned OnCustomerDespawned;
-
     //public List<GameObject> waypoints;
-
     public float speed = 4;
-
     //public Transform target;
-
     public Vector3 target;
-
     public Vector3 target2;
-
     public GameObject OrderIndicator;
-
     public MeshRenderer PLS;
+    //public int count;
+    Rigidbody CustomerRB;
 
+    private int collisionCount = 0;
+
+    public bool IsNotColliding
+    {
+        get { return collisionCount == 0; }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        collisionCount++;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        collisionCount--;
+    }
 
     // Start is called before the first frame update
-
     void Start()
-
     {
-
         PLS.enabled = false;
-
-        //transform.position = target.position;
-
-        //transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-
-        //transform.position = transform.position + new Vector3(0, 0, 0);
-
-        //setRigidbodyState(true);
-
-        //setColliderState(false);
-
-        //GetComponent<Animator>().enabled = true;
-
         //float step = speed * Time.deltaTime;
-
+        //transform.position = Vector3.MoveTowards(transform.position, target, step);
+        //transform.position = target.position;
+        //transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        //transform.position = transform.position + new Vector3(0, 0, 0);
+        //setRigidbodyState(true);
+        //setColliderState(false);
+        //GetComponent<Animator>().enabled = true;
+        //float step = speed * Time.deltaTime;
         //transform.position = Vector3.MoveTowards(transform.position, target2, step);
-
     }
-
 
     public void despawn()
-
     {
-
         //setRigidbodyState(false);
-
         //setColliderState(true);
 
-
         if (gameObject != null)
-
         {
-
             Destroy(gameObject, 3f);
-
         }
-
 
         if (OnCustomerDespawned != null)
-
         {
-
             OnCustomerDespawned();
-
         }
-
     }
-
 
     /*void setRigidbodyState(bool state)
-
     {
-
         Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
-
-
         foreach (Rigidbody rigidbody in rigidbodies)
-
         {
-
             rigidbody.isKinematic = state;
-
         }
-
-
         GetComponent<Rigidbody>().isKinematic = !state;
-
     }*/
-
 
     // Update is called once per frame
-
     void Update()
-
     {
-
         float step = speed * Time.deltaTime;
-
         transform.position = Vector3.MoveTowards(transform.position, target, step);
-
+        /*if (collisionCount == 0)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+        }*/
         //transform.position = Vector3.MoveTowards(transform.position, target2, step);
-
         /*int index = 0;
-
         Vector3 newPos = Vector3.MoveTowards(transform.position, waypoints[index].transform.position, speed * Time.deltaTime);
-
         transform.position = newPos;*/
-
     }
 
-
     /*void DestroyGameObject()
-
     {
-
         Destroy(gameObject);
-
     }*/
 
-
     private void OnTriggerEnter(Collider other)
-
     {
-
         if (other.tag == "Register")
-
         {
-
             PLS.enabled = true;
-
+            //count++;
             //Destroy(GameObject.FindGameObjectWithTag("OrderIndicator"));
-
             //Destroy(other.gameObject);
-
             //DestroyGameObject();
-
             print("destroyed order indicator");
-
         }
-
         else if (other.tag == "Player")
-
         {
-
+            print("collided with player");
+            float step = speed * Time.deltaTime;
             PLS.enabled = false;
-
-
+            transform.position = Vector3.MoveTowards(transform.position, target2, step);
+        }     
+        else if (other.tag == "Customer")
+        {
+            print("customer in the way");
+            CustomerRB.velocity = Vector3.zero;
+            //RigidBody.IsKinematic = false;
+            //GetComponenet<Rigidbody>().velocity = Vector3.zero;
+            //return;
         }
+        /*else
+        {
+            print("move toward target");
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+        }*/
+        //return;
     }
 }
--- FastFoodTycoonProject/Assets/Scripts/CustomerSpawn.cs
+++ FastFoodTycoonProject/Assets/Scripts/CustomerSpawn.cs
@@ -1,111 +1,78 @@
 using System.Collections;
-
 using System.Collections.Generic;
-
 using UnityEngine;
-
 
 public class CustomerSpawn : MonoBehaviour
 
 {
+    //public Quaternion rotation;
+    //Transform target;
 
     //Attempt #3
-
-
     /*public GameObject CustomerPrefab;
-
-
     public void Spawn()
-
     {
-
         GameObject Customer = (GameObject)Instantiate(CustomerPrefab, transform.position, transform.rotation);
-
     }*/
-
 
 
     //Attempt #2, spawns a customer, when customer despawns a new one spawns
-
     public Transform m_SpawnPoints;
-
     public GameObject m_CustomerPrefab;
-
+    public float secondsBetweenSpawn;
+    public float elapsedTime = 0.0f;
 
     private void Start()
-
     {
-
         SpawnNewCustomer();
-
     }
-
 
     private void OnEnable()
-
     {
-
         CustomerController.OnCustomerDespawned += SpawnNewCustomer;
-
     }
-
 
     void SpawnNewCustomer()
-
     {
-
-        Instantiate(m_CustomerPrefab, m_SpawnPoints.transform.position, Quaternion.identity);
-
+        Instantiate(m_CustomerPrefab, m_SpawnPoints.transform.position, transform.rotation);
+        //transform.Rotate(new Vector3(axis2, axis1, 0), Space.World);
     }
-
 
 
     //Attempt #1
-
     /*[SerializeField]
-
     private GameObject CustomerPrefab;
 
-
     [SerializeField]
-
     private float CustomerInterval = 3.5f;
 
-
     // Start is called before the first frame update
-
     void Start()
-
     {
-
         StartCoroutine(SpawnCustomer(CustomerInterval, CustomerPrefab));
-
-    }
-
-
-    // Update is called once per frame
-
-    void Update()
-
-    {
-
-        
-
-    }
-
-
-    private IEnumerable SpawnCustomer(float interval, GameObject Customer)
-
-    {
-
-        yield return new WaitForSeconds(interval);
-
-        GameObject newCustomer = Instantiate(Customer, new Vector3(5, 0, 0), Quaternion.identity);
-
-        StartCoroutine(SpawnCustomer(interval, Customer));
-
     }*/
 
+    // Update is called once per frame
+    void Update()
+    {
+        /*elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > secondsBetweenSpawn) 
+        {
+            elapsedTime = 0;
+            Debug.Log(true);
+            SpawnNewCustomer();
+            //Vector3 spawnPosition = new Vector3(4.71f, 0.72f, 0f);
+            //GameObject newEnemy = (GameObject)Instantiate(enemyObject, spawnPosition, Quaternion.Euler(0, 0, 0));
+        }*/
+    }
+
+    /*private IEnumerable SpawnCustomer(float interval, GameObject Customer)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newCustomer = Instantiate(Customer, new Vector3(5, 0, 0), Quaternion.identity);
+        StartCoroutine(SpawnCustomer(interval, Customer));
+    }*/
 }
 
 
